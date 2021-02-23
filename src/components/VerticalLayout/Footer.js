b320d8c943de648e6af7e14dae013025841c6b2a
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2021-02-23 09:02:45 +0100

    minor: customize footer

--- src/components/VerticalLayout/Footer.js
+++ src/components/VerticalLayout/Footer.js
@@ -7,10 +7,9 @@ const Footer = () => {
       <footer className="footer">
         <Container fluid={true}>
           <Row>
-            <Col md={6}>{new Date().getFullYear()} © Skote.</Col>
-            <Col md={6}>
-              <div className="text-sm-right d-none d-sm-block">
-                Design & Develop by Themesbrand
+            <Col md={12}>
+              <div className="text-sm-center d-none d-sm-block">
+                {new Date().getFullYear()}, BigFoot
               </div>
             </Col>
           </Row>

30ab683d92e39f7d4716c0ddd14f6d8c819d7407
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2021-03-10 08:14:02 +0100

    rename scss

--- src/App.js
+++ src/App.js
@@ -19,11 +19,11 @@ import NonAuthLayout from "./components/NonAuthLayout"
 import "./assets/scss/theme.scss"
 
 
-// Import for eleven finance scss 
+// Import for bigfoot scss 
 // Note:
-// - these file "eleven-main.scss" will override the css rules from the theme we use (skote)
+// - these file "bigfoot.scss" will override the css rules from the theme we use (skote)
 // - if you need to modify any css, add your rules on that file (to avoid changing the theme files) 
-import "./assets/scss/eleven/eleven-main.scss"
+import "./assets/scss/bigfoot/bigfoot.scss"
 
 
 

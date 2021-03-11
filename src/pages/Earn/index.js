4b770417ef2a67f6e634caedd02ec25c2aa97f7e
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2021-03-11 09:17:37 +0100

    earn: extract lending options to a separate file

--- src/pages/Earn/index.js
+++ src/pages/Earn/index.js
@@ -1,4 +1,5 @@
 import React, {useState} from "react"
+import lendingOptions from '../../data/lendingOptions'
 import {
   Container,
   Row,
@@ -21,64 +22,7 @@ import { Link } from "react-router-dom"
 
 const Earn = props => {
 
-  const [assets, setassets] = useState([
-    {
-      icon: "mdi mdi-ethereum",
-      color: "",
-      title: "ibETHv2",
-      assetSymbol: "ETH",
-      apy: "1.58",
-      supply: "17,224.89",
-      supplyInDollars: "28,734,381.43",
-      borrow: "15,519.46",
-      borrowInDollars: "25,889,404.33",
-      utilization: "90.10",
-      balance: "0.00",
-      balanceDiff: "+0.00",
-    },
-    {
-      icon: "mdi mdi-currency-btc",
-      color: "",
-      title: "ibETHv2",
-      assetSymbol: "BTC",
-      apy: "1.58",
-      supply: "17,224.89",
-      supplyInDollars: "28,734,381.43",
-      borrow: "15,519.46",
-      borrowInDollars: "25,889,404.33",
-      utilization: "90.10",
-      balance: "0.00",
-      balanceDiff: "+0.00",
-    },
-    {
-      icon: "mdi mdi-ethereum",
-      color: "",
-      title: "ibETHv2",
-      assetSymbol: "ETH",
-      apy: "1.58",
-      supply: "17,224.89",
-      supplyInDollars: "28,734,381.43",
-      borrow: "15,519.46",
-      borrowInDollars: "25,889,404.33",
-      utilization: "90.10",
-      balance: "0.00",
-      balanceDiff: "+0.00",
-    },
-    {
-      icon: "mdi mdi-currency-php",
-      color: "",
-      title: "ibETHv2",
-      assetSymbol: "PHP",
-      apy: "1.58",
-      supply: "17,224.89",
-      supplyInDollars: "28,734,381.43",
-      borrow: "15,519.46",
-      borrowInDollars: "25,889,404.33",
-      utilization: "90.10",
-      balance: "0.00",
-      balanceDiff: "+0.00",
-    },
-  ]);
+  const [assets, setassets] = useState(lendingOptions);
 
   const [ isModalOpen, setisModalOpen] = useState(false);
 

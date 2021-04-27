f624cf89819d061d060968398e7340a7d07436d1
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2021-04-28 00:21:09 +0200

    update contract addresses

--- src/data/addresses/addresses.js
+++ src/data/addresses/addresses.js
@@ -2,19 +2,19 @@
 export const addressMasterChef = "0x1ac6C0B955B6D7ACb61c9Bdf3EE98E0689e07B8A";
 export const addressBfBNB = "0xA96C90223e4cC69192A9ffF1BA4c8b86D02765B2";
 export const addressBigfoot11Cake = "";
-export const addressBigfoot11CakeBnb = "0x662a29061f5aA31296e24db14b54a2C134Bd41e8";
+export const addressBigfoot11CakeBnb = "0x7fF89d5d048DA9a090C51D3FF7eD0fC45bcFe521";
 
 //strategies
 export const addressStrategyZapperAddSingleAsset = "";
-export const addressStrategyZapperAdd = "0x63fcBd5F43b5640DDCEEF07AC2309F66F1495924";
-export const addressStrategyLiquidation11xxxBnb = "0xFA8331C59e7A0C86A2E8C530639aA45b34a6CA7D";
+export const addressStrategyZapperAdd = "0xCb84813806225D629dFb9C651A3d600955135998";
+export const addressStrategyLiquidation11xxxBnb = "0x2d679e20A5E2461E6ADCa390DCD30c583F85B5b1";
 
 //eleven
 export const address11Cake = "0xE050ACc68c778b6F7F63bDfeCDEc576FcC6D2274";
-export const address11CakeBnb = "0x093fe062705700BE2e4EffE45Be3e5563E397b20";
+export const address11CakeBnb = "0x58D25A7e34eE8fA7A070510e6D2E0096Ed62c828";
 
 //pancake
-export const addressPancakeWbnbBusdLp = "0x1b96b92314c44b159149f7e0303511fb2fc4774f";	
+export const addressPancakeWbnbBusdLp = "0x1B96B92314C44b159149f7E0303511fB2Fc4774f";	
 export const addressWbnb = "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c";
 export const addressBusd = "0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56";
 export const addressCakeBnbLp = "0xA527a61703D82139F8a06Bc30097cC9CAA2df5A6";

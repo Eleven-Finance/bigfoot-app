1c9478a099226546c0361ca900c82e68f296bda3
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2021-05-14 23:12:50 +0200

    update bfusd and bf addresses

--- src/data/addresses/addresses.js
+++ src/data/addresses/addresses.js
@@ -1,10 +1,10 @@
 //bigfoot
 export const addressMasterChef = "0x1ac6C0B955B6D7ACb61c9Bdf3EE98E0689e07B8A";
 export const addressBfBNB = "0xA96C90223e4cC69192A9ffF1BA4c8b86D02765B2";
-export const addressBfUSD = "0x8ec5A2b90A25ACe47f2869227350F9f3fc433Ece";
+export const addressBfUSD = "0xc9a1C3073CeA3Fade1406a6C72Fb91A8be0725B2";
 export const addressBigfoot11Cake = "";
 export const addressBigfoot11CakeBnb = "0x7fF89d5d048DA9a090C51D3FF7eD0fC45bcFe521";
-export const addressBigfoot11UsdtBusd = "0x54Ed6235dd62ECE31f0Fa16B74c23806204C2ff0";
+export const addressBigfoot11UsdtBusd = "0xe8e6AD531D2073423418a89438F8e73B4076C127";
 
 //strategies
 export const addressStrategyZapperAddSingleAsset = "";
